7484c48edb98de96de785028f9e7a5caa012957e
Torey Hickman <torey@toreyhickman.com>
2015-08-31 10:19:03 -0500

    Reformat file

--- refactor.rb
+++ refactor.rb
@@ -1,12 +1,13 @@
-##### 1. Read this [article on code smells](http://martinfowler.com/bliki/CodeSmell.html)
-
-##### 2. Reference this [list of smells and refactorings](http://www.industriallogic.com/wp-content/uploads/2005/09/smellstorefactorings.pdf)
- 
-
-##### 3. List each smell that is dealt with by the "Extract Method" refactoring
+=begin
+Release 0:  List the smells that can be combated by extracting a method.
 
 
-##### 4. Find a piece of code you wrote earlier that has one of these smells. Place the old code below and name the smell.
+Release 1:  Find code that has one or more of the listed smells. Copy the old code below and name the smell.
 
- 
-##### 5. Apply the Extract Method refactoring to clean up the smell. Place the new code below.
+
+=end
+
+
+
+
+# Release 2:  Apply the extract method refactoring to clean up the smells.
